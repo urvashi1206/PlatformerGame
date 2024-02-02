7a315fe87e22b20c2311af9cab3f4361b78d16a3
1TaodeOgden1 <70358247+1TaodeOgden1@users.noreply.github.com>
2024-02-02 15:34:13 -0500

    playing around with platforms

--- Assets/UI/Scripts/StartScreen.cs
+++ Assets/UI/Scripts/StartScreen.cs
@@ -2,23 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour
 {
+    //audio source for the start button
+    AudioSource _audioPlayer;
+    private bool startReady = false;
+    [SerializeField]
+    GameObject startButton; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _audioPlayer = startButton.GetComponent<AudioSource>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //check that the button has been pressed AND that the audioSource isn't running.
+        if(!_audioPlayer.isPlaying && startReady)
+        {
+            SceneManager.LoadScene("TestScene");
+        }
+     
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene("TestScene");
+        _audioPlayer.Play();
+        //prevents player from spamming the button and preventing the game from starting (since the audio won't stop)
+        startButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        //allows sound to fully play before switching scenes.
+        startReady = true; 
+
     }
 }
