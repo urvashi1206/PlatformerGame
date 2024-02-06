20d09b416588a4f16c70f1ce617159d694d056c2
urvashi1206 <44489378+urvashi1206@users.noreply.github.com>
2024-02-06 01:04:45 -0500

    Progress bar and player impact

--- Assets/UI/Scripts/PlayerHUD.cs
+++ Assets/UI/Scripts/PlayerHUD.cs
@@ -6,23 +6,16 @@ using UnityEngine.UI;
 public class PlayerHUD : MonoBehaviour
 {
     
-    Image healthbar;  //the red health bar sprite
+    
     Transform[] manaIcons; //the blue sprites that represent the player's mana; 
     [SerializeField]
     float maxHealth, maxMana; 
 
     private void Start()
     {
-        healthbar = GameObject.Find("bar").GetComponent<Image>();
+        //healthbar = GameObject.Find("bar").GetComponent<Image>();
 
         manaIcons = GameObject.Find("Mana").GetComponentsInChildren<Transform>();
-    }
-
-    //value should never be set above max health
-    public void SetLife(float value)
-    {
-        Debug.Log(value);
-        healthbar.fillAmount = value / maxHealth;
     }
 
     //value should never be set above max mana
