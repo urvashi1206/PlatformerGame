e62d7fc64f605a009f66cd3e05e98e2ef74dc016
urvashi1206 <44489378+urvashi1206@users.noreply.github.com>
2024-02-12 14:02:27 -0500

    Added disappearing platform

---
+++ Assets/Scripts/Components/Disappear_Platform.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear_Platform : MonoBehaviour
+{
+    public float fadeDelay = 3f;
+    public float alphaValue = 0;
+    public bool destroyGameObject = false;
+
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        destroyGameObject = true;
+        StartCoroutine(FadeTo(alphaValue, fadeDelay));
+    }
+
+    IEnumerator FadeTo(float aValue, float fadeTime)
+    {
+        float alpha = spriteRenderer.color.a;
+
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / fadeTime)
+        {
+            Color newColor = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, Mathf.Lerp(alpha, aValue, t));
+            spriteRenderer.color = newColor;
+            yield return null;
+        }
+
+        if(destroyGameObject)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
